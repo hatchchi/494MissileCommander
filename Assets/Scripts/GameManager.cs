d0a7a28161da8e603116d9de185f1600b37cc670
hatchchi <phatch2@uic.edu>
2022-09-12 15:12:39 -0500

    first upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ships")]
+    public GameObject[] ships;
+    public EnemyScript enemyScript;
+    private ShipScript shipScript;
+    private List<int[]> enemyShips;
+    private int shipIndex = 0;
+    public List<TileScript> allTileScripts;
+
+    [Header("HUD")]
+    public Button nextBtn;
+    public Button rotateBtn;
+    public Button replayBtn;
+    public Text topText;
+    public Text playerShipText;
+    public Text enemyShipText;
+
+    [Header("Objects")]
+    public GameObject missilePrefab;
+    public GameObject enemyMissilePrefab;
+    public GameObject firePrefab;
+    public GameObject woodDock;
+    //
+    public GameObject tile;
+
+    private bool setupComplete = false;
+    private bool playerTurn = true;
+
+    private List<GameObject> playerFires = new List<GameObject>();
+    private List<GameObject> enemyFires = new List<GameObject>();
+
+    private int enemyShipCount = 5;
+    private int playerShipCount = 5;
+
+    // VOICE  //////////////////////////////////
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    //////////////////////////////////
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipScript = ships[shipIndex].GetComponent<ShipScript>();
+        nextBtn.onClick.AddListener(() => NextShipClicked());
+        rotateBtn.onClick.AddListener(() => RotateClicked());
+        replayBtn.onClick.AddListener(() => ReplayClicked());
+        enemyShips = enemyScript.PlaceEnemyShips();
+
+        // VOICE
+        actions.Add("forward", Forward); //worked
+       // actions.Add("bee", Forward);//worked on low
+      //  actions.Add("cee", Forward);//worked on low
+      //  actions.Add("dee", Forward); //worked on low
+      //  actions.Add("ee", Forward); //failed on low
+      //  actions.Add("ef", Forward); //worked on low
+      //  actions.Add("gee", Forward); //worked
+      //  actions.Add("aych", Forward); //worked on low
+      //  actions.Add("eye", Forward); //worked on low
+      //  actions.Add("B3", Forward); //worked
+      //  actions.Add("D 4", Forward); //nope
+        actions.Add("Deefor", Forward); //worked on low
+        actions.Add("I 2", Forward); //worked on low
+        actions.Add("shoot at J2", Forward); //worked
+        actions.Add("fire at G3", Forward); //nope
+        //actions.Add("fire at D4", Forward); //nope
+         // actions.Add("kill D 4", Forward); //nope
+        actions.Add("drop at B3", Forward); //worked
+
+        ////https://docs.unity3d.com/ScriptReference/Windows.Speech.KeywordRecognizer.html
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray(), ConfidenceLevel.Low);
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+
+        //////////////////////////////////
+    }
+
+    // VOICE //////////////////////////////////
+    public void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    public void Forward()
+    {
+        if (setupComplete && playerTurn)
+        {
+           
+            Vector3 tilePos = tile.transform.position;
+            
+            // 2.25 per tile in each direction. 
+            //set new bomb starting position.
+            tilePos.y = 15 ;
+            tilePos.x = 9 ;
+            tilePos.z = 9 ;
+            playerTurn = false;
+            Instantiate(missilePrefab, tilePos, missilePrefab.transform.rotation);
+        }
+    }
+    //////////////////////////////////
+
+
+    private void NextShipClicked()
+    {
+        if (!shipScript.OnGameBoard())
+        {
+            shipScript.FlashColor(Color.red);
+        }
+        else
+        {
+            if (shipIndex <= ships.Length - 2)
+            {
+                shipIndex++;
+                shipScript = ships[shipIndex].GetComponent<ShipScript>();
+                shipScript.FlashColor(Color.yellow);
+            }
+            else
+            {
+                rotateBtn.gameObject.SetActive(false);
+                nextBtn.gameObject.SetActive(false);
+                woodDock.SetActive(false);
+                topText.text = "Guess an enemy tile.";
+                setupComplete = true;
+                for (int i = 0; i < ships.Length; i++) ships[i].SetActive(false);
+            }
+        }
+
+    }
+
+    public void TileClicked(GameObject tile)
+    {
+        if (setupComplete && playerTurn)
+        {
+            Vector3 tilePos = tile.transform.position;
+            tilePos.y += 15;
+            playerTurn = false;
+            Instantiate(missilePrefab, tilePos, missilePrefab.transform.rotation);
+        }
+        else if (!setupComplete)
+        {
+            PlaceShip(tile);
+            shipScript.SetClickedTile(tile);
+        }
+    }
+
+    private void PlaceShip(GameObject tile)
+    {
+        shipScript = ships[shipIndex].GetComponent<ShipScript>();
+        shipScript.ClearTileList();
+        Vector3 newVec = shipScript.GetOffsetVec(tile.transform.position);
+        ships[shipIndex].transform.localPosition = newVec;
+    }
+
+    void RotateClicked()
+    {
+        shipScript.RotateShip();
+    }
+
+    public void CheckHit(GameObject tile)
+    {
+        int tileNum = Int32.Parse(Regex.Match(tile.name, @"\d+").Value);
+        int hitCount = 0;
+        foreach (int[] tileNumArray in enemyShips)
+        {
+            if (tileNumArray.Contains(tileNum))
+            {
+                for (int i = 0; i < tileNumArray.Length; i++)
+                {
+                    if (tileNumArray[i] == tileNum)
+                    {
+                        tileNumArray[i] = -5;
+                        hitCount++;
+                    }
+                    else if (tileNumArray[i] == -5)
+                    {
+                        hitCount++;
+                    }
+                }
+                if (hitCount == tileNumArray.Length)
+                {
+                    enemyShipCount--;
+                    topText.text = "SUNK!!!!!!";
+                    enemyFires.Add(Instantiate(firePrefab, tile.transform.position, Quaternion.identity));
+                    tile.GetComponent<TileScript>().SetTileColor(1, new Color32(68, 0, 0, 255));
+                    tile.GetComponent<TileScript>().SwitchColors(1);
+                }
+                else
+                {
+                    topText.text = "HIT!!";
+                    tile.GetComponent<TileScript>().SetTileColor(1, new Color32(255, 0, 0, 255));
+                    tile.GetComponent<TileScript>().SwitchColors(1);
+                }
+                break;
+            }
+
+        }
+        if (hitCount == 0)
+        {
+            tile.GetComponent<TileScript>().SetTileColor(1, new Color32(38, 57, 76, 255));
+            tile.GetComponent<TileScript>().SwitchColors(1);
+            topText.text = "Missed, there is no ship there.";
+        }
+        Invoke("EndPlayerTurn", 1.0f);
+    }
+
+    public void EnemyHitPlayer(Vector3 tile, int tileNum, GameObject hitObj)
+    {
+        enemyScript.MissileHit(tileNum);
+        tile.y += 0.2f;
+        playerFires.Add(Instantiate(firePrefab, tile, Quaternion.identity));
+        if (hitObj.GetComponent<ShipScript>().HitCheckSank())
+        {
+            playerShipCount--;
+            playerShipText.text = playerShipCount.ToString();
+            enemyScript.SunkPlayer();
+        }
+        Invoke("EndEnemyTurn", 2.0f);
+    }
+
+    private void EndPlayerTurn()
+    {
+        for (int i = 0; i < ships.Length; i++) ships[i].SetActive(true);
+        foreach (GameObject fire in playerFires) fire.SetActive(true);
+        foreach (GameObject fire in enemyFires) fire.SetActive(false);
+        enemyShipText.text = enemyShipCount.ToString();
+        topText.text = "Enemy's turn";
+        enemyScript.NPCTurn();
+        ColorAllTiles(0);
+        if (playerShipCount < 1) GameOver("ENEMY WINs!!!");
+    }
+
+    public void EndEnemyTurn()
+    {
+        for (int i = 0; i < ships.Length; i++) ships[i].SetActive(false);
+        foreach (GameObject fire in playerFires) fire.SetActive(false);
+        foreach (GameObject fire in enemyFires) fire.SetActive(true);
+        playerShipText.text = playerShipCount.ToString();
+        topText.text = "Select a tile";
+        playerTurn = true;
+        ColorAllTiles(1);
+        if (enemyShipCount < 1) GameOver("YOU WIN!!");
+    }
+
+    private void ColorAllTiles(int colorIndex)
+    {
+        foreach (TileScript tileScript in allTileScripts)
+        {
+            tileScript.SwitchColors(colorIndex);
+        }
+    }
+
+    void GameOver(string winner)
+    {
+        topText.text = "Game Over: " + winner;
+        replayBtn.gameObject.SetActive(true);
+        playerTurn = false;
+    }
+
+    void ReplayClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,95 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class TileScript : MonoBehaviour
+{
+
+    
+    GameManager gameManager;
+    Ray ray;
+    RaycastHit hit;
+    //vid 2-13.57
+    private bool missileHit = false;
+    Color32[] hitColor = new Color32[2];
+    
+    void Start()
+    {
+        
+
+        //vid 2-16.39
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        hitColor[0] = gameObject.GetComponent<MeshRenderer>().material.color;
+        hitColor[1] = gameObject.GetComponent<MeshRenderer>().material.color;
+    }
+
+
+
+
+
+
+    void Update()
+    {
+
+
+        //vid 2-16.57
+        // define ray, as from cmaeras perspective, input the mouse position assigining it to an xy point (mousePosition)
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // to pay attention only to correct tile
+        if (Physics.Raycast(ray, out hit))
+        {
+            //Debug.Log("ray ____________________" + ray); // provides 3 coordinates of mouse when on board
+            //check its the tile that was selected through the mouse
+            //is mouse button down AND is the name of what we just collided (with ray) the same as this item (gameObject.name)
+
+
+            if (Input.GetMouseButtonDown(0) && hit.collider.gameObject.name == gameObject.name)
+            {
+
+                //Debug.Log("gameObject.name ____________________" + gameObject.name); //"Tile (32)"
+
+
+                if (missileHit == false)
+                //if square not already used?
+                {
+
+                    //check collider name  (vid 3-12.37)
+                    //pass to the game manager, the hit object, the tile that was just clicked. 
+                    //therefore this is where it reports which tile was chosen (and not whether theres a ship there yet)
+                    //Debug.Log("hit.collider.gameObject ____________________" + hit.collider.gameObject.name); //"Tile (32)"
+                    gameManager.TileClicked(hit.collider.gameObject);
+
+                }
+            }
+        }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Missile"))
+        {
+            missileHit = true;
+        }
+        else if (collision.gameObject.CompareTag("EnemyMissile"))
+        {
+            hitColor[0] = new Color32(38, 57, 76, 255);
+            GetComponent<Renderer>().material.color = hitColor[0];
+        }
+                
+    }
+
+    public void SetTileColor(int index, Color32 color)
+    {
+        hitColor[index] = color;
+    }
+
+    public void SwitchColors(int colorIndex)
+    {
+        GetComponent<Renderer>().material.color = hitColor[colorIndex];
+    }
+}
