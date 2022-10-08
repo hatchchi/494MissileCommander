2144dda5d6d6f15861d9c3ddc93c019b3593c1e1
Paul Hatch <@phatch2@uic.edu@>
2022-10-08 14:21:25 -0500

    updated layout

--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -39,25 +39,33 @@ public class ShipScript : MonoBehaviour
 
     public Vector3 GetOffsetVec(Vector3 tilePos)
     {
-        return new Vector3(tilePos.x + xOffset, 2, tilePos.z + zOffset);
+        //return new Vector3(tilePos.x + xOffset, 20, tilePos.z + zOffset);
+        Debug.Log("_____________(shipscript)___________________________________tilePos value is_" + tilePos);
+        return new Vector3(tilePos.x - 62 + xOffset, 20, tilePos.z -47 + zOffset);  //62 and 47 is how much off the ships are in the new positions
+
+        // y value is height ship appears before falling onto board
     }
 
     public void RotateShip()
     {
-        if (clickedTile == null) return;
-        touchTiles.Clear();
-        transform.localEulerAngles += new Vector3(0, 0, nextZRotation);
-        nextZRotation *= -1;
-        float temp = xOffset;
-        xOffset = zOffset;
-        zOffset = temp;
+        if (clickedTile == null) return;  //clickedtile is active tile xyz 
+        touchTiles.Clear();  //Reset touched tiles count
+        transform.localEulerAngles += new Vector3(0, 0, nextZRotation); //rotate by 90
+        
+        nextZRotation *= -1;  //next time will rotate -90
+        
+        //switch x offset value with z offset value
+        float temp = xOffset; 
+        xOffset = zOffset;  
         SetPosition(clickedTile.transform.position);
+        Debug.Log("rotate command_______");
     }
 
     public void SetPosition(Vector3 newVec)
     {
         ClearTileList();
-        transform.localPosition = new Vector3(newVec.x + xOffset, 2, newVec.z + zOffset);
+        transform.localPosition = new Vector3(newVec.x -62 + xOffset, 20, newVec.z -47 + zOffset); 
+        Debug.Log("this is where Setposition() was used");
     }
 
     public void SetClickedTile(GameObject tile)
